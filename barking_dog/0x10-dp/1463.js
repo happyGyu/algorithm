b3105676c6c73ff75b79c41e2e3f9a9249501460
happyGyu <happyboyto.dev@gmail.com>
2022-02-13 13:15:15 +0900

    [수정] BFS 풀이 추가

--- barking_dog/0x10-dp/1463.js
+++ barking_dog/0x10-dp/1463.js
@@ -9,3 +9,29 @@ for (let i = 2; i <= num; i++) {
     dp[i] = Math.min(...temp);
 }
 console.log(dp[num]);
+
+//bfs풀이
+const queue = [1];
+const log = [];
+log[1] = 0;
+
+while (true) {
+    const curr = queue.shift();
+    const temp = [curr * 2, curr * 3, curr + 1];
+    if (temp.indexOf(num) !== -1) {
+        console.log(log[curr] + 1);
+        return;
+    }
+    if (curr * 2 < num && log[curr * 2] === undefined) {
+        log[curr * 2] = log[curr] + 1;
+        queue.push(curr * 2);
+    } 
+    if (curr * 3 < num && log[curr * 3] === undefined) {
+        log[curr * 3] = log[curr] + 1;
+        queue.push(curr * 3);
+    } 
+    if (curr + 1 < num && log[curr + 1] === undefined) {
+        log[curr + 1] = log[curr] + 1;
+        queue.push(curr + 1);
+    } 
+}
