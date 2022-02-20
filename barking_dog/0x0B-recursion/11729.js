e12b8e4d15f80dddf8cca98d19caf06e57b7f6f0
happyGyu <happyboyto.dev@gmail.com>
2022-02-20 23:32:36 +0900

    수정: 하노이 탑 문제풀이 수정(완)

--- barking_dog/0x0B-recursion/11729.js
+++ barking_dog/0x0B-recursion/11729.js
@@ -1,16 +1,18 @@
 //const num = Number(require('fs').readFileSync('/dev/stdin').toString());
-const num = 4;
+const num = 3;
 
-let answer = '';
+let answer = [];
 function solution(a,b,n) {
     if (n === 1) {
-        answer += `${a} ${b}\n`;
+        answer.push(`${a} ${b}`);
         return;
     }
     solution(a, 6 - a - b, n - 1);
-    answer += `${a} ${b}\n`;
+    answer.push(`${a} ${b}`);
     solution(6 - a - b, b, n - 1);
 }
 
 solution(1,3,num);
-console.log(answer);
+const final = answer.join('\n');
+console.log(answer.length);
+console.log(final);
