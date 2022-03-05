0800cfcd036801890e628e5ef8204c13072f0eac
happyGyu <happyboyto.dev@gmail.com>
2022-03-05 17:28:01 +0900

    풀이 수정:문제를 잘못 읽어 잘못된 초기조건 줬던 것 수정

--- baekjoon/study/0304/21735.js
+++ baekjoon/study/0304/21735.js
@@ -1,13 +1,16 @@
 const input = require('fs').readFileSync('./test.txt').toString().trim().split('\n');
+
 const [len, time] = input[0].trim().split(' ').map(n => Number(n));
 const snows = input[1].trim().split(' ').map(n => Number(n));
+
 const caseNum = 2 ** (time);
 
 let score = 0;
 for (let i = 0; i < caseNum; i++) {
     const currCase = i.toString(2).padStart(time, '0');
+    console.log(currCase)
     let pointer = -1;
-    let currScore = 0;
+    let currScore = 1;
     for (let step of currCase) {
         if (step === '1') {
             pointer += 2;
