e508c2e416d6a59a9c46bf9458a53bcf245aa305
happyGyu <happyboyto.dev@gmail.com>
2022-04-06 23:18:37 +0900

    풀이수정: reduce 문법 활용

--- baekjoon/study/greedy/0406/1541_s2.js
+++ baekjoon/study/greedy/0406/1541_s2.js
@@ -1,9 +1,6 @@
 const group = require('fs').readFileSync('./test.txt').toString().trim().split('-');
 const parsedGroup = group.map(e => e.trim().split('+').map(n => Number(n)));
 const summedGroup = parsedGroup.map(group => group.reduce((acc, n) => acc + n));
-let answer = summedGroup[0];
-for (let i = 1; i < summedGroup.length; i++) {
-    answer -= summedGroup[i];
-}
+let answer = summedGroup.reduce((acc, n) => acc - n);
 console.log(answer);
 
