03b61c68b49a561b2797095b0444fa1903487026
happyGyu <happyboyto.dev@gmail.com>
2022-05-17 23:49:31 +0900

    새풀이: 조이스틱. 반쪽짜리 풀이

---
+++ programmers/greedy/조이스틱.js
@@ -0,0 +1,36 @@
+function solution(name) {
+  const distance = {
+    A: 0,
+    B: 1,
+    C: 2,
+    D: 3,
+    E: 4,
+    F: 5,
+    G: 6,
+    H: 7,
+    I: 8,
+    J: 9,
+    K: 10,
+    L: 11,
+    M: 12,
+    N: 13,
+    O: 12,
+    P: 11,
+    Q: 10,
+    R: 9,
+    S: 8,
+    T: 7,
+    U: 6,
+    V: 5,
+    W: 4,
+    X: 3,
+    Y: 2,
+    Z: 1
+  };
+  let answer = 0;
+  for (const c of name) {
+    answer += distance[c];
+  }
+  answer += name.length - 1;
+  return answer;
+}
