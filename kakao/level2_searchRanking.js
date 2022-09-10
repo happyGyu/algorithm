bc7e305aacb2a06283a689ee9e1df767cd059688
happyGyu <happyboyto.dev@gmail.com>
2022-09-10 22:42:17 +0900

    재풀이: 순위검색 문제 재시도(성공)

--- kakao/level2_searchRanking.js
+++ kakao/level2_searchRanking.js
@@ -33,7 +33,6 @@ function solution(info, query) {
 }
 
 //Info 별로 검색에 걸릴수 있는 query의 종류는 총 16개. 그렇기 때문에 모든 info의 점수를 각각 16개의 key에 할당한 후 query 수행을 효율적으로 하기 위한 풀이
-// 그러나 효율성 검사를 한개도 통과하지 못함. 어디서 실수가 있는지 정확 테스트도 몇개 통과를 못하는데, 애초에 효율성을 통과하지 못하므로 패스.
 function solution(info, query) {
   const answer = [];
   const infoHash = {};
@@ -59,19 +58,27 @@ function solution(info, query) {
       }
     }
   });
-
+    
+  Object.keys(infoHash).forEach(key => {
+    infoHash[key].sort((a, b) => a - b);
+  });
+    
   for (const currQuery of query) {
     const splited = currQuery.split(" ");
     const targetScore = Number(splited.pop());
     const hashKey = splited.join(" ");
     const filteredScores = infoHash[hashKey];
-    const currAnswer = filteredScores.reduce((acc, score) => {
-      if (score >= targetScore) {
-        return acc + 1;
-      }
-      return acc;
-    }, 0);
-    answer.push(currAnswer);
+    if (!filteredScores) {
+        answer.push(0);
+        continue;
+    }
+    let [left, right] = [0, filteredScores.length];
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      filteredScores[mid] >= targetScore ? (right = mid) : (left = mid + 1);
+    }
+
+    answer.push(filteredScores.length - left);
   }
 
   return answer;
