371ccd18f65a573c5d8e27a4b58fb6280c4a24bd
happyGyu <happyboyto.dev@gmail.com>
2022-09-23 19:23:55 +0900

    수정: 럭키스트레이트 백준 스타일에 맞게 수정

--- nadongbin/implement/previous/lucky_straight.js
+++ nadongbin/implement/previous/lucky_straight.js
@@ -1,10 +1,10 @@
-const input = 7755;
+const input = +require('fs').readFileSync('./input.txt').toString();
 
 function solution(input) {
-    const arr = String(input).split('');
-    const front = arr.slice(0,arr.length / 2).map(n=>parseInt(n)).reduce((acc, n) => acc + n);
-    const rear = arr.slice(arr.length/2).map(n=>parseInt(n)).reduce((acc,n) => acc + n);
-    return (front === rear)? 'LUCKY' : 'READY';
+  const arr = String(input).split('');
+  const front = arr.slice(0, arr.length / 2).reduce((acc, n) => acc + Number(n), 0);
+  const rear = arr.slice(arr.length / 2).reduce((acc, n) => acc + Number(n), 0);
+  return front === rear ? 'LUCKY' : 'READY';
 }
 
 console.log(solution(input));
